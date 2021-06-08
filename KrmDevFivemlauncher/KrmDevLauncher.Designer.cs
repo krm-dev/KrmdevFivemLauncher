cc61984327994ef7e5952787ec1db4ff0f29fbaf
KrmDev <krmdevelopmnt@gmail.com>
2021-06-08 22:01:54 +0300

    Add files via upload

---
+++ KrmDevFivemlauncher/KrmDevLauncher.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace KrmDevFivemlauncher
+{
+    partial class KrmDevLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KrmDevLauncher));
+            this.btnclose = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnts = new System.Windows.Forms.Button();
+            this.btnoyna = new System.Windows.Forms.Button();
+            this.btndc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.Color.Transparent;
+            this.btnclose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnclose.BackgroundImage")));
+            this.btnclose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnclose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnclose.FlatAppearance.BorderSize = 0;
+            this.btnclose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnclose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnclose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnclose.Location = new System.Drawing.Point(1127, 12);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(41, 38);
+            this.btnclose.TabIndex = 5;
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(1077, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(42, 38);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 50);
+            this.panel1.TabIndex = 7;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // btnts
+            // 
+            this.btnts.BackColor = System.Drawing.Color.Transparent;
+            this.btnts.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnts.BackgroundImage")));
+            this.btnts.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnts.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnts.FlatAppearance.BorderSize = 0;
+            this.btnts.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnts.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnts.Location = new System.Drawing.Point(494, 575);
+            this.btnts.Name = "btnts";
+            this.btnts.Size = new System.Drawing.Size(222, 70);
+            this.btnts.TabIndex = 30;
+            this.btnts.UseVisualStyleBackColor = false;
+            this.btnts.Click += new System.EventHandler(this.btnts_Click);
+            // 
+            // btnoyna
+            // 
+            this.btnoyna.BackColor = System.Drawing.Color.Transparent;
+            this.btnoyna.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnoyna.BackgroundImage")));
+            this.btnoyna.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnoyna.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnoyna.FlatAppearance.BorderSize = 0;
+            this.btnoyna.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnoyna.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnoyna.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnoyna.Location = new System.Drawing.Point(722, 495);
+            this.btnoyna.Name = "btnoyna";
+            this.btnoyna.Size = new System.Drawing.Size(214, 87);
+            this.btnoyna.TabIndex = 29;
+            this.btnoyna.UseVisualStyleBackColor = false;
+            this.btnoyna.Click += new System.EventHandler(this.btnoyna_Click);
+            // 
+            // btndc
+            // 
+            this.btndc.BackColor = System.Drawing.Color.Transparent;
+            this.btndc.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btndc.BackgroundImage")));
+            this.btndc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btndc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndc.FlatAppearance.BorderSize = 0;
+            this.btndc.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btndc.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btndc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndc.Location = new System.Drawing.Point(944, 575);
+            this.btndc.Name = "btndc";
+            this.btndc.Size = new System.Drawing.Size(227, 70);
+            this.btndc.TabIndex = 28;
+            this.btndc.UseVisualStyleBackColor = false;
+            this.btndc.Click += new System.EventHandler(this.btndc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 629);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = ".KrmDev Launcher";
+            // 
+            // KrmDevLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1180, 655);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnts);
+            this.Controls.Add(this.btnoyna);
+            this.Controls.Add(this.btndc);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "KrmDevLauncher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KrmDev Fivem Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnts;
+        private System.Windows.Forms.Button btnoyna;
+        private System.Windows.Forms.Button btndc;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KrmDevFivemlauncher/KrmDevLauncher.cs
@@ -0,0 +1,86 @@
+﻿//KrmDev Launcher
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace KrmDevFivemlauncher
+{
+    public partial class KrmDevLauncher : Form
+    {
+        int mov;
+        int movX;
+        int movY;
+        public KrmDevLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            foreach (var islem in Process.GetProcessesByName("Fivem"))
+                islem.Kill();
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnts_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("ts3server://{ts3serverip}/?9987&nickname="); //ts3 server ip yazın. nickname bölümüne de oyuncular =*** yazdığınız nickname ile ts3 e bağlanırlar. 
+        }
+
+        private void btndc_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/DAVETLİNK"); //discord davet linkiniz ile değiştirin.
+        }
+
+        private void btnoyna_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://cfx.re/join/SERVERBAĞLANTIURLSİ"); //fivem short urlniz ile değiştirin.
+        }
+    }
+}
+
+
+
+
+//KrmDev Launcher
